a4775ebfbfdae30f0d334b2dc112d2b5110552da
Neil Barnwell <mailbox@neilbarnwell.co.uk>
2015-06-03 13:38:55 +0100

    Added NSB implementations

---
+++ OrderProcessor.Client.NSB/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NServiceBus;
+using OrderProcessor.Client.Core;
+using OrderProcessor.Messages;
+using OrderProcessor.Messages.V1;
+
+namespace OrderProcessor.Client.NSB
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var configuration = new BusConfiguration();
+            var conventions = configuration.Conventions();
+            conventions.DefiningMessagesAs(type => type.Assembly == typeof(OrderPlaced).Assembly);
+            var bus = Bus.CreateSendOnly(configuration);
+
+            ColorConsole.WriteLine("Enter customer reference and press ENTER:");
+
+            string input;
+            while (!string.IsNullOrWhiteSpace(input = Console.ReadLine()))
+            {
+                ColorConsole.WriteLine("Sending message...");
+                bus.Send(
+                    new OrderPlaced
+                    {
+                        OrderId = Guid.NewGuid(),
+                        CustomerReference = input
+                    });
+            }
+        }
+    }
+}
---
+++ OrderProcessor.Server.NSB/Ordering/PlaceOrderHandler.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using NServiceBus;
+using OrderProcessor.Client.Core;
+using OrderProcessor.Messages;
+using OrderProcessor.Messages.V1;
+
+namespace OrderProcessor.Server.NSB.Ordering
+{
+    public class PlaceOrderHandler : IHandleMessages<OrderPlaced>
+    {
+        public void Handle(OrderPlaced message)
+        {
+            ColorConsole.WriteLine(ConsoleColor.Green, "Handling message: {0}", message.ToString());
+        }
+    }
+}
